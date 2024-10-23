8cbfacbac48d39d72ad7df647db4a4a4081122da
Murilo Brandini Zague <zaguemurilo7@gmail.com>
2024-10-23 14:53:45 -0300

    padding ajustments

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import LOGO from '../assets/logo.png'
 export default function Header() {
     return(
         <div>
-            <div className="pl-16 p-6">
+            <div className="pl-16 p-5">
                 <img src={LOGO} alt="" width={280} className='ml-8 select-none'/>
             </div>
             <div className="lineorange h-5">
