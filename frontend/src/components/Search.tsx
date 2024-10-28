956d4472f5e493fd38a8bb212deb5f49f9405a62
Murilo Brandini Zague <zaguemurilo7@gmail.com>
2024-10-28 13:17:22 -0300

    input de pesquisa fix tablets

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -5,14 +5,14 @@ export default function Search() {
     return(
         <section>
             <div className="flex flex-row gap-20 tsm:gap-8 tsm:flex-col">
-                <div className="w-1/2 tsm:w-full tsm:text-center">
+                <div className="w-1/2 lg:w-2/3 tsm:w-full tsm:text-center">
                     <p className="text-4xl tsm:text-2xl lg:text-3xl tsm:tracking-normal font-medium inter tracking-wider">Encontre A 
                     <span className="textorange"> Melhor<br/> Receita</span> Para A Sua Fome
                     </p>
                 </div>
-                <div className="flex w-2/3 searchbar tsm:w-full">
+                <div className="flex w-2/3 searchbar tsm:w-full lg:items-center tlg:items-center">
                     <input type="text" placeholder="Procure por uma receita" 
-                    className="bg-red-500 w-3/4 rounded-full text-xl search mt-4 tsm:mt-0 outline-none tsm:p-4 lg:w-full lg:text-lg tsm:w-full tsm:text-base tsm:pl-16"/>
+                    className="bg-red-500 w-3/4 rounded-full text-xl search mt-4 tsm:mt-0 outline-none tsm:p-4 tlg:mt-0 lg:w-full tlg:h-16 lg:h-16 lg:text-lg tsm:w-full tsm:text-base tsm:pl-16"/>
 
                     <img src={FILTER} className='w-9 h-9 mt-8 ml-6 tsm:hidden lg:hidden tmd:hidden tlg:hidden' alt="" />
                 </div>
