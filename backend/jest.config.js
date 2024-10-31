aa429065353b75e328908e040e54d37062d208c9
jpmoncao <pedrohjoao44@gmail.com>
2024-10-31 14:00:01 -0300

    chore: update config to run tests

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,15 +1,13 @@
 export default {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    transform: {
-        '^.+\\.ts$': 'ts-jest',
-    },
-    extensionsToTreatAsEsm: ['.ts'],
-    globals: {
-        '^.+\\.ts$': ['ts-jest', { useESM: true }],
-    },
-    moduleNameMapper: {
-        '^(\\.{1,2}/.*)\\.js$': '$1',
-    },
-    testMatch: ['**/*.spec.ts'],
-}
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  transform: {
+    "^.+\\.ts?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  }
+};
