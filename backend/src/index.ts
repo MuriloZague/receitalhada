084e7cc11b9b01301311e429c8f3ea7e028acc39
jpmoncao <pedrohjoao44@gmail.com>
2024-10-31 14:15:00 -0300

    feat: API main file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,22 @@
-import express, { Express, Request } from "express";
+import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
 
+// Inicializa o uso de dotenv no arquivo
+dotenv.config();
+
+// Obtém a porta da API através do arquivo .env (padrão 3000)
+const PORT = process.env.API_PORT ?? 3000;
+
+// Cria instância do Express e declara o uso de json no servidor
 const app: Express = express();
+app.use(express.json());
 
-app.get('/up', (req: Request, res: Response) => res.json({ message: 'Server is running!' }))
+// Rota para testar a conectividade da API
+app.get('/up', (req: Request, res: Response) => {
+    res.status(202).json({ message: "Server is up!" })
+});
 
-app.listen(3000, () => console.log('Server is runnig!'))
+// Cria o servidor na porta passada e exibe o log
+const server = app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`));
+
+export { app, server };
