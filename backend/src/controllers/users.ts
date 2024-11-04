9cfc1fd9c0cadb9f6b099d6b436e8782da46a33a
jpmoncao <pedrohjoao44@gmail.com>
2024-11-04 10:33:02 -0300

    feat: create with password and authenticate user

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -1,5 +1,7 @@
 import Controller from './controller.js';
 import { Request, Response } from "express";
+// import UserRequest from '../types/Request.js';
+import jwt from 'jsonwebtoken';
 
 import { prisma } from '../index.js';
 import { Prisma, Users } from '@prisma/client';
@@ -8,6 +10,8 @@ import { ErrorCode } from '../errors/errors.js';
 import AppError from '../errors/app-error.js';
 
 import { isValidDDD } from '../utils/phone.js';
+import { decrypt, encrypt } from '../utils/hash.js';
+import { auth } from '../utils/auth.js';
 
 class UserController extends Controller<Users> {
     constructor() { super(); }
@@ -17,6 +21,8 @@ class UserController extends Controller<Users> {
 
         const result = await this.handler(async () => {
             this.validate(data);
+
+            data.password = await encrypt(data.password);
 
             const user = await prisma.users.create({ data });
             return user;
@@ -80,6 +86,31 @@ class UserController extends Controller<Users> {
         return this.handleResponse(res, result);
     }
 
+    public async authUser(req: Request, res: Response): Promise<Response> {
+        const result = await this.handler(async () => {
+            const data: Users = req.body;
+            let user: Users;
+
+            if (data.email)
+                user = await prisma.users.findUniqueOrThrow({ where: { email: data.email } });
+            else if (data.username)
+                user = await prisma.users.findUniqueOrThrow({ where: { username: data.username } });
+            else
+                throw new AppError('The authentication credentials have not been entered', ErrorCode.VALIDATION_ERROR);
+
+            const passwordsIsEquals = await decrypt(data.password, user.password);
+            if (!passwordsIsEquals)
+                throw new AppError('The entered password is not equal to the user\'s password', ErrorCode.VALIDATION_ERROR);
+
+            const { secret, expiresIn } = auth;
+            const token = jwt.sign({ id_user: user.id_user }, secret, { expiresIn })
+
+            return { token };
+        });
+
+        return this.handleResponse(res, result);
+    }
+
     public validate(data: Users) {
         // Verifica formato do email
         const emailSplited: string[] = data.email.split('@');
@@ -94,6 +125,10 @@ class UserController extends Controller<Users> {
         // Verifica tamanho do telefone + DDD
         if (data.phone.length !== 11)
             throw new AppError('Invalid phone format. Please ensure your enter a valid phone number, such as (11)99988776655', ErrorCode.VALIDATION_ERROR);
+
+        const regexPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$_*&@#]{8,}$/;
+        if (!regexPattern.test(data.password))
+            throw new AppError('Invalid password, must contain at least 8 characters, 1 uppercase and lowercase letter, 1 number and 1 special letter', ErrorCode.VALIDATION_ERROR);
     }
 }
 
