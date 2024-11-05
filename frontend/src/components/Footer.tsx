2ad1e625279b0f3abf9c5bad8492797e852eecb9
Murilo Brandini Zague <zaguemurilo7@gmail.com>
2024-11-05 10:03:22 -0300

    style: footer added

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer(){
     return(
-        <section className="bg-orange-500 h-80 flex items-end justify-center shadow-[inset_0px_8px_10px_rgba(0,0,0,0.3)]">
+        <section className="bg-orange-500 h-80 flex items-end justify-center shadow-[inset_0px_8px_10px_rgba(0,0,0,0.2)]">
             <div>
                 <p className="text-white">Direitos por Receitalhada ©</p>
             </div>
