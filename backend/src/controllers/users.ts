2541e3977231b57b9f539aaacfb07bd788476297
jpmoncao <pedrohjoao44@gmail.com>
2024-11-05 13:13:03 -0300

    feat: forgot password

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -115,7 +115,7 @@ class UserController extends Controller<Users> {
         return this.handleResponse(res, result);
     }
 
-    public async authUser(req: Request, res: Response): Promise<Response> {
+    public async authenticateUser(req: Request, res: Response): Promise<Response> {
         const result = await this.handler(async () => {
             const data: Users = req.body;
             let user: Users;
@@ -143,20 +143,20 @@ class UserController extends Controller<Users> {
         return this.handleResponse(res, result);
     }
 
-    public async authEmail(req: Request, res: Response): Promise<Response> {
+    public async authenticateEmailCode(req: Request, res: Response): Promise<Response> {
         const result = await this.handler(async () => {
-            const { id_user, authenticate_code } = req.body;
+            const { email, authenticate_code } = req.body;
 
             if (!authenticate_code)
                 throw new AppError('Authenticate code is not found', ErrorCode.VALIDATION_ERROR);
 
             const user = await prisma.users.findUniqueOrThrow({ where: { authenticate_code } });
 
-            if (user.id_user != Number(id_user))
+            if (user.email != email)
                 throw new AppError('Authenticate code is invalid', ErrorCode.VALIDATION_ERROR);
 
             await prisma.users.update({
-                where: { id_user: user.id_user },
+                where: { email: user.email },
                 data: { authenticate_code: null, register_at: new Date() }
             });
 
@@ -168,24 +168,161 @@ class UserController extends Controller<Users> {
         return this.handleResponse(res, result);
     }
 
-    public validate(data: Users) {
+    public async sendForgotPasswordCode(req: Request, res: Response): Promise<Response> {
+        const MAX_ATTEMPTS = 3;
+        const RESET_TIME = 60 * 60 * 1000;
+        const FIFTEEN_MINUTES = 15 * 60 * 1000;
+        const NOW = Date.now();
+
+        let data = req.body;
+
+        const result = await this.handler(async () => {
+            this.validate(data);
+
+            const forgotPasswordCode = generateAuthCode();
+            await sendMail(data.email, 'Restauração de senha', 'Código de restauração de senha: ' + forgotPasswordCode);
+
+            await prisma.$transaction(async (prisma) => {
+                let user = await prisma.users.findUnique({
+                    where: { email: data.email }
+                });
+
+                if (!user) {
+                    throw new AppError('User not found', ErrorCode.RECORD_NOT_FOUND_ERROR);
+                }
+
+                if (user.forgot_password_attempted_at &&
+                    user.forgot_password_attempted_at.getTime() + RESET_TIME < NOW) {
+                    await prisma.users.update({
+                        where: { email: data.email },
+                        data: {
+                            forgot_password_attempts: 0,
+                            forgot_password_attempted_at: null
+                        }
+                    });
+                } else {
+                    if (user.forgot_password_attempts >= MAX_ATTEMPTS) {
+                        throw new AppError('You have exceeded password recovery attempts', ErrorCode.VALIDATION_ERROR);
+                    }
+                }
+
+                const forgotPasswordExpiresAt = new Date(Date.now() + FIFTEEN_MINUTES);
+                user = await prisma.users.update({
+                    where: { email: data.email },
+                    data: {
+                        forgot_password_code: forgotPasswordCode,
+                        forgot_password_expires_at: forgotPasswordExpiresAt,
+                        forgot_password_attempts: { increment: 1 },
+                        forgot_password_attempted_at: new Date()
+                    }
+                });
+            });
+
+            return { message: 'An authentication code was sent to your email, please check your inbox' };
+        });
+
+        return this.handleResponse(res, result);
+    }
+
+    public async authenticateForgotPasswordCode(req: Request, res: Response): Promise<Response> {
+        const NOW = new Date();
+        let { email, forgot_password_code, password } = req.body;
+
+        const result = await this.handler(async () => {
+            if (!forgot_password_code)
+                throw new AppError('Authenticate code is not found', ErrorCode.VALIDATION_ERROR);
+
+            const user = await prisma.users.findUniqueOrThrow({ where: { forgot_password_code } });
+
+            if (user.email != email)
+                throw new AppError('Authenticate code is invalid', ErrorCode.VALIDATION_ERROR);
+
+            if (NOW > user.forgot_password_expires_at)
+                throw new AppError('Authenticate code is expired', ErrorCode.VALIDATION_ERROR);
+
+            this.validate({ email, password });
+
+            password = await encrypt(password);
+
+            await prisma.users.update({
+                where: { email: user.email },
+                data: {
+                    password,
+                    forgot_password_code: null,
+                    forgot_password_expires_at: null,
+                    forgot_password_attempts: 0,
+                    forgot_password_attempted_at: null
+                }
+            });
+
+            return {
+                'message': 'User\'s forgot password request was accepted! Password was edited'
+            };
+        })
+
+        return this.handleResponse(res, result)
+    }
+
+    public async editPassword(req: Request, res: Response): Promise<Response> {
+        let data = req.body;
+
+        const result = await this.handler(async () => {
+            this.validate(data);
+
+            const forgotPasswordCode = generateAuthCode();
+            await sendMail(data.email, 'Restauração de senha', 'Código de resturação de senha: ' + forgotPasswordCode)
+
+            return { message: 'A authentication code was send in your email, please check your inbox' };
+        })
+
+        return this.handleResponse(res, result)
+    }
+
+    /*public async editUser(req: UserRequest, res: Response): Promise<Response> {
+        const id_user = req.user.id_user;
+        const data = req.body;
+
+        const result = this.handler(async () => {
+            this.validate(data);
+
+            if (!data)
+                throw new AppError('There is no data to change', ErrorCode.VALIDATION_ERROR);
+
+            const user: Users = await prisma.users.update({
+                where: { id_user },
+                data
+            });
+
+            return user;
+        });
+
+        return this.handleResponse(res, result);
+    }*/
+
+    public validate(data: any) {
         // Verifica formato do email
-        const emailSplited: string[] = data.email.split('@');
-        if (emailSplited.length < 2 || emailSplited[1].trim() == '')
-            throw new AppError('Invalid email format. Please ensure you enter a valid email address, such as example@domain.com', ErrorCode.VALIDATION_ERROR);
+        if (data.email) {
+            const emailSplited: string[] = data.email.split('@');
+            if (emailSplited.length < 2 || emailSplited[1].trim() == '')
+                throw new AppError('Invalid email format. Please ensure you enter a valid email address, such as example@domain.com', ErrorCode.VALIDATION_ERROR);
+        }
 
         // Verifica o DDD
-        const DDD = data.phone.slice(0, 2);
-        if (!isValidDDD(DDD))
-            throw new AppError('Invalid DDD code', ErrorCode.VALIDATION_ERROR);
+        if (data.phone) {
+            const DDD = data.phone.slice(0, 2);
+            if (!isValidDDD(DDD))
+                throw new AppError('Invalid DDD code', ErrorCode.VALIDATION_ERROR);
 
-        // Verifica tamanho do telefone + DDD
-        if (data.phone.length !== 11)
-            throw new AppError('Invalid phone format. Please ensure your enter a valid phone number, such as (11)99988776655', ErrorCode.VALIDATION_ERROR);
+            // Verifica tamanho do telefone + DDD
+            if (data.phone.length !== 11)
+                throw new AppError('Invalid phone format. Please ensure your enter a valid phone number, such as (11)99988776655', ErrorCode.VALIDATION_ERROR);
+        }
 
-        const regexPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$_*&@#]{8,}$/;
-        if (!regexPattern.test(data.password))
-            throw new AppError('Invalid password, must contain at least 8 characters, 1 uppercase and lowercase letter, 1 number and 1 special letter', ErrorCode.VALIDATION_ERROR);
+        if (data.password) {
+            const regexPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$_*&@#]{8,}$/;
+            if (!regexPattern.test(data.password))
+                throw new AppError('Invalid password, must contain at least 8 characters, 1 uppercase and lowercase letter, 1 number and 1 special letter', ErrorCode.VALIDATION_ERROR);
+        }
     }
 }
 
--- backend/src/routers/users.ts
+++ backend/src/routers/users.ts
@@ -17,14 +17,17 @@ class UserRouter extends Router {
 
     protected setRoutes(): void {
         this.router.post('/', async (req: Request, res: Response) => { this.controller.createUser(req, res) });
-        this.router.post('/auth', async (req: Request, res: Response) => { this.controller.authUser(req, res) });
-        this.router.post('/email-auth', async (req: Request, res: Response) => { this.controller.authEmail(req, res) });
+        this.router.post('/auth', async (req: Request, res: Response) => { this.controller.authenticateUser(req, res) });
+        this.router.post('/email-auth', async (req: Request, res: Response) => { this.controller.authenticateEmailCode(req, res) });
+        this.router.post('/forgot-password', async (req: Request, res: Response) => { this.controller.sendForgotPasswordCode(req, res) });
+        this.router.post('/reset-password', async (req: Request, res: Response) => { this.controller.authenticateForgotPasswordCode(req, res) });
 
         this.router.use(AuthenticateToken);
 
         this.router.get('/', async (req: Request, res: Response) => { this.controller.listAllUsers(req, res) });
         this.router.get('/signed', async (req: UserRequest, res: Response) => { this.controller.listUserSigned(req, res) });
         this.router.get('/:id', async (req: UserRequest, res: Response) => { this.controller.listUserById(req, res) });
+        // this.router.head('/edit', async (req: UserRequest, res: Response) => { this.controller.editUser(req, res) });
     }
 }
 
