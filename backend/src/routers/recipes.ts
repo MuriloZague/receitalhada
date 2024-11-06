18aa012f433d361b6bdedbed4dac13104e7660d4
jpmoncao <pedrohjoao44@gmail.com>
2024-11-06 15:54:51 -0300

    feat: rotas para listar receitas

--- backend/src/routers/recipes.ts
+++ backend/src/routers/recipes.ts
@@ -4,6 +4,7 @@ import UserRequest from '../types/Request.js';
 
 import RecipeController from '../controllers/recipes.js';
 import AuthenticateToken from '../middlewares/authenticate-token.js';
+import OptionalAuthenticateToken from '../middlewares/optional-authenticate-token.js';
 
 export class RecipeRouter extends Router {
   private controller: RecipeController;
@@ -14,6 +15,12 @@ export class RecipeRouter extends Router {
   }
 
   protected setRoutes(): void {
+    this.router.get('/', OptionalAuthenticateToken, async (req: UserRequest, res: Response) => {
+      this.controller.listAllRecipes(req, res);
+    });
+    this.router.get('/:id', OptionalAuthenticateToken, async (req: UserRequest, res: Response) => {
+      this.controller.listRecipeById(req, res);
+    });
 
     this.router.use(AuthenticateToken);
 
